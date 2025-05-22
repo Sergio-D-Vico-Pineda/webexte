bf092c9c10151464bf3dde49586dde471cc7ffb3
Sergio David Vico Pineda <sergiodavid.vico@ipinformaticaprofesional.com>
2025-05-22 18:40:19 +0200

    refactor: update styles and add title attributes to inputs
    - Added global styles for margin, padding, and box-sizing in styles.css
    - Enhanced body styling with new classes for title-container, title, and btn-section
    - Updated button styles for add-btn and clear-btn with hover effects
    - Added title attributes to all input elements in prueba.html for better accessibility

---
+++ notused/js.js
@@ -0,0 +1,10 @@
+// Check the next sibling for a label
+/* let label = input.nextElementSibling;
+if (label && label.tagName.toLowerCase() === 'label') {
+    const labelText = label.textContent.trim();
+    if (labelText.toLowerCase().includes(searchText.toLowerCase())) {
+        console.log('a: ' + searchText)
+        matchCount++;
+        lastMatchedInput = input;
+    }
+} */
